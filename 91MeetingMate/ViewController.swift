577fde8c24a82ab80005794c7dd954d9dee926e3
Roman <romazan.26@gmail.com>
2026-02-13 09:44:11 +0300

    Initial Commit

---
+++ 91MeetingMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  91MeetingMate
+//
+//  Created by Роман Главацкий on 13.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
